8f4ffb17328d3d39b54940dc78b50fe919ae9d3d
Daniela Huppenkothen <d.huppenkothen@uva.nl>
2026-07-15 15:21:52 +0200

    made myself a little script to help me run each element one by one

---
+++ docs/run_element.sh
@@ -0,0 +1,25 @@
+
+ELEMENT=1
+
+DATADIR=/home/dhuppenkot2/data/spexai_data/data/element_$ELEMENT
+CACHE=/home/dhuppenkot2/data/spexai_data/processed/element$ELEMENT
+RUNS=/home/dhuppenkot2/data/spexai_data/runs/element$ELEMENT/recipe_t04
+
+mkdir $CACHE
+mkdir $RUNS
+
+python scripts/preprocess_spectra.py \
+    --datadir $DATADIR \
+    --outdir  $CACHE
+
+ 
+nohup python -m spexai.train.train_adaptive \
+    --mode reweight --n_train 0 --pr_mix 0.4 --use_linehead off \
+    --schedule wsd --steps 100000 --eval_every 2000 --tag reweight_full \
+    --cachedir $CACHE \
+    --outdir  $RUNS \
+    > $RUNS/recipe_t04.log 2>&1 &
+
+python benchmark_operator.py --rundir $RUNS/recipe_t04 --cachedir $CACHE
+
+python benchmark_instruments.py --linehead_ckpt $RUNS/reweight_full.pt --cachedir $CACHE --rundir $RUNS --responses_dir /home/dhuppenkot2/data/spexai_data/responses
